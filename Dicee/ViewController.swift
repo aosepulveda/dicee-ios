be96648d3f0bfa3ffb9d851795adae4491815d07
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2017-12-24 19:40:55 -0300

    first commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Alejandro Oyarzún Sepúlveda on 12/24/17.
+//  Copyright © 2017 Alejandro Oyarzún Sepúlveda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
