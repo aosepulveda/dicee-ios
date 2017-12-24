1a898562844fe48a2d878d2160d3aefe2b65a309
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2017-12-24 20:12:47 -0300

    added two random numbers

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,8 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
 
+    // change appearance of a element
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +27,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // interact with UI
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex1)
+    }
+    
 }
 
